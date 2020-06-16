8c541d8e6e31f37c0756453fdf745b5fd7553789
pmontoyag <juanpamontoya@hotmail.com>
2020-06-15 23:40:46 -0500

    Commit inicial

---
+++ PrimerProyecto/PrimerProyecto/Controllers/ConexionController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Npgsql;
+
+namespace PrimerProyecto.Controllers
+{
+    public class ConexionController : Controller
+    {
+        NpgsqlConnection conexion = new NpgsqlConnection();
+
+        // GET: Conexion
+        public  void AbrirConexion()
+        {
+            try
+            {
+                conexion.ConnectionString = Core.@Properties.Settings.Default.DBCS + ";Password=pmontoya";
+                conexion = new NpgsqlConnection(conexion.ConnectionString);
+                conexion.Open();
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            try
+            {
+                conexion.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // GET: Conexion/Details/5
+        public string Estado()
+        {
+            
+            return conexion.State.ToString(); 
+        }
+
+        // GET: Conexion/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Conexion/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Conexion/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Conexion/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Conexion/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Conexion/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PrimerProyecto/PrimerProyecto/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Npgsql;
+
+namespace PrimerProyecto.Controllers
+{
+    public class HomeController : Controller
+    {
+        ConexionController conexion = new ConexionController();
+
+        public ActionResult Index()
+        {
+            try
+            {
+                conexion.AbrirConexion();
+
+                string estado = conexion.Estado();
+
+                conexion.CerrarConexion();
+            }
+            catch (Exception e)
+            {
+                conexion.CerrarConexion();
+                Console.WriteLine(e.Message);
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
